c3e1556d431b9c3102d5b106974ba76e4f0c7135
Timothy Pettingill <timothy.pettingill@jetblue.com>
2022-04-01 09:11:07 -0600

    Create `databricks_stack_example` package

---
+++ databricks_stack_example/__init__.py
@@ -0,0 +1,4 @@
+import sys
+import logging
+
+logging.basicConfig(fmt="%(asctime)s - %(levelname)s - %(pathname)s - %(message)s", stream=sys.stdout)
---
+++ databricks_stack_example/scripts/task1.py
@@ -0,0 +1,9 @@
+from argparse import ArgumentParser
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(prog="task1")
+    parser.add_argument("--param-1", metavar="ANY", help="the first parameter")
+    parser.add_argument("--param-2", metavar="INT", type=int, help="the second parameter")
+    args = parser.parse_args()
+    print(f'Task 1 executed with parameters: {vars(args)}')
